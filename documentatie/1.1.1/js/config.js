11d941a022b3f71b220e57b99adadd6983e4c42a
Inge La Rivière <3697652+zoot-inge@users.noreply.github.com>
2026-09-15 09:38:30 +0200

    naar actuele versie met minimale aanpassingen

---
+++ documentatie/1.1.1/js/config.js
@@ -0,0 +1,31 @@
+var respecConfig = {
+  specStatus: "DEF",
+  specType: "ST",
+  title: "Geo-BAG berichtenverkeer",
+  publishVersion: "1.1.1",
+  //canonicalURI: "WV",
+  editors: [
+    {
+      name: "Arnoud de Boer",
+      company: "Geonovum",
+      companyURL: "http://www.geonovum.nl/",
+      mailto: "a.deboer@geonovum.nl",
+      note: ""    
+    }  
+  ],
+  shortName: "berichtenverkeer",
+  pubDomain: "geobag", 
+  previousURI: "https://docs.geostandaarden.nl/geobag/def-st-berichtenverkeer-20171110/",
+  github: "https://github.com/Geonovum/geobag",
+  previousPublishDate: "2017-08-01",
+  previousStatus: "DEF",
+  //localBiblio: {
+  //  "ISO19136": {
+  //    "href": "https://www.iso.org/standard/32554.html",
+  //    "title": "ISO 19136:2007 Geographic information -- Geography Markup Language (GML)",
+  //    "authors": [""],
+  //    "date": "September 2007",
+  //    "publisher": "International Organization for Standardization",
+  //  },
+  //},
+};
